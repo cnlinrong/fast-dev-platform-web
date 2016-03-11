919a71f353717362f19a479f50f385e23e3b1b42
rong.lin <cnlinrong@gmail.com>
2016-03-11 16:54:48 +0800

    修改数据库用户表结构。

--- db/fast-dev-platform-db.sql
+++ db/fast-dev-platform-db.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50711
 File Encoding         : 65001
 
-Date: 2016-03-10 17:44:35
+Date: 2016-03-11 16:54:03
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,18 +22,19 @@ DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
   `id` varchar(32) NOT NULL COMMENT '主键',
   `username` varchar(50) NOT NULL COMMENT '用户名',
-  `real_name` varchar(50) DEFAULT NULL COMMENT '真名',
   `password` varchar(50) NOT NULL COMMENT '密码',
+  `real_name` varchar(50) DEFAULT NULL COMMENT '真实姓名',
   `phone` varchar(11) DEFAULT NULL COMMENT '手机号码',
-  `email` varchar(100) DEFAULT NULL,
-  `sex` tinyint(1) DEFAULT NULL COMMENT '性别',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `sex` tinyint(1) DEFAULT NULL COMMENT '性别：1为男，0为女',
   `address` varchar(100) DEFAULT NULL COMMENT '地址',
   `create_time` varchar(100) NOT NULL COMMENT '创建日期',
-  `update_time` varchar(100) DEFAULT NULL COMMENT '更新日期',
+  `update_time` varchar(100) NOT NULL COMMENT '更新日期',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('74D4A272A665414FA2CE0E06EBA7801E', 'abc', '烦烦烦', '96E79218965EB72C92A549DD5A330112', '15666666666', null, '1', '啊啊啊啊', '1457602811928', '1457602811928');
+INSERT INTO `t_user` VALUES ('5011E3809D114711A4E98B05F2EBCCE5', 'abc', 'E10ADC3949BA59ABBE56E057F20F883E', null, null, null, '1', null, '1457682932094', '1457682953033');
+INSERT INTO `t_user` VALUES ('D563B6F67FAE49689AF603B6029E4D59', 'test', '96E79218965EB72C92A549DD5A330112', null, null, null, '1', null, '1457680523644', '1457682836369');
